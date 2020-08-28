111fb6689c0db4c0b8f8c6c08e08a388a1996545
Nick Lavery <tuneboynick@gmail.com>
2020-08-27 21:31:44 -0400

    ReactResources component attempt

--- src/components/ReactResources.js
+++ src/components/ReactResources.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function ReactResources (props) {
+    const { links } = props;
+    if (!links.length) {
+        return <span className="empty">Sad and empty, need more resources!</span>;
+    }
+
+    return (
+        <ol className="links">
+            {links.map(links => <ReactResources key={link} value={link} />)}
+        </ol>
+    );
+}
+
 // https://reactjs.org/
+
