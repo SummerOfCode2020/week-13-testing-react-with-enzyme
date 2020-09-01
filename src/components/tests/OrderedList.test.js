48a1c12be719afeb4de1832805339cadd85c2610
jessicareynolds <reynolds.jessann@gmail.com>
2020-09-01 17:34:21 -0400

    Update test for text value

--- src/components/tests/OrderedList.test.js
+++ src/components/tests/OrderedList.test.js
@@ -19,7 +19,7 @@ describe('Order List Generic List Component', () => {
         const wrapper = shallow(<OrderedList options={animals} />);
 
         expect(wrapper.find('.empty').exists()).toBe(true);
-        expect(wrapper.text('.empty')).toEqual('Nothing to see here. Move along.');
+        expect(wrapper.text()).toEqual('Nothing to see here. Move along.');
     });
 
     it('renders a single animal option', () => {
